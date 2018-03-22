637a032f6983a973a238bf3beef1896ed2322c93
Tony Lin <tonylin1208@gmail.com>
2018-03-22 14:12:00 +0800

    add index and show in api_v1 controller

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -1,2 +1,30 @@
 class Api::V1::PhotosController < ApiController
+  def index
+    @photos = Photo.all
+    render json: {
+      data: @photos.map do |photo|
+        {
+          title: photo.title,
+          date: photo.date,
+          description: photo.description
+        }
+      end
+    }
+  end
+
+  def show
+    @photo = Photo.find_by(params[:id])
+    if !@photo
+      render json: {
+        message: "Can't find the photo!",
+        status: 400
+      }
+    else
+      render json: {
+        title: @photo.title,
+        date: @photo.date,
+        description: @photo.description
+      }
+    end
+  end
 end
